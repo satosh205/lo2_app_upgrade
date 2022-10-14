bd154afed0d237ef3ca261aeee3b03a9aecdf6aa
Prince <vishwakarmaprince833@gmail.com>
2022-10-14 18:50:16 +0530

    changed config fiel

--- lib/utils/config.dart
+++ lib/utils/config.dart
@@ -1,19 +1,21 @@
-const APK_DETAILS = {
-  "app_name": "MasterG",
-  "package_name": "com.at.masterg",
-  "logo_url": "masterg_app_bar.svg",
-  "theme_image_url": "masterg_logo.svg",
-  "theme_image_url2": "signupimage.gif",
-  "theme_color": "FDB515",
-  "element_color": "#FFFFFF",
-  "nlms_api_key": "0612b32b39f4b29f48c5c5363028ee916bb99MasterG",
-  "domain_url": "https://qa.learningoxygen.com/",
-  "enable_boarding_screen": "0",
-  "splash_image": "masterg_logo.svg",
-  "isBrandEnabled": "1",
-  "faqEnabled": "0",
-  "policy_url": "https://qa.learningoxygen.com/policy"
-};
+// const APK_DETAILS = {
+//   "app_name": "MasterG",
+//   "package_name": "com.at.masterg",
+//   "logo_url": "masterg_app_bar.svg",
+//   "theme_image_url": "masterg_logo.svg",
+//   "theme_image_url2": "signupimage.gif",
+//   "theme_color": "FDB515",
+//   "element_color": "#FFFFFF",
+//   "nlms_api_key": "0612b32b39f4b29f48c5c5363028ee916bb99MasterG",
+//   "domain_url": "https://qa.learningoxygen.com/",
+//   "enable_boarding_screen": "0",
+//   "splash_image": "masterg_logo.svg",
+//   "isBrandEnabled": "1",
+//   "faqEnabled": "0",
+//   "policy_url": "https://qa.learningoxygen.com/policy"
+// };
+
+
 
 
 
@@ -35,23 +37,22 @@ const APK_DETAILS = {
 //   "policy_url": "https://qa.learningoxygen.com/policy"
 // };
 
-// const APK_DETAILS = {
-//   "app_name": "learn_build",
-//   "package_name": "com.learn_build",
-//   "logo_url": "logo_lb_1.png",
-//   "theme_image_url": "lnb_banner_1.png",
-//   "theme_image_url2": "lnb_banner_2.jpg",
-
-//   "theme_color": "#1663fe",
-//   "element_color": "#1663fe",
-//   "nlms_api_key": "0612b32b39f4b29f48c5c5363028ee916bb99TECH",
-//   "domain_url": "https://mylearning.learnandbuild.in/",
-//   "enable_boarding_screen": "1",
-//   "splash_image": "logo_lb_1.png",
-//   "isBrandEnabled": "0",
-//   "faqEnabled": "1",
-//   "policy_url": "https://learnandbuild.in/privacy-policy/"
-// };
+const APK_DETAILS = {
+  "app_name": "learn_build",
+  "package_name": "com.learn_build",
+  "logo_url": "logo_lb_1.png",
+  "theme_image_url": "lnb_banner_1.png",
+  "theme_image_url2": "lnb_banner_2.jpg",
+  "theme_color": "#1663fe",
+  "element_color": "#1663fe",
+  "nlms_api_key": "0612b32b39f4b29f48c5c5363028ee916bb99TECH",
+  "domain_url": "https://mylearning.learnandbuild.in/",
+  "enable_boarding_screen": "1",
+  "splash_image": "logo_lb_1.png",
+  "isBrandEnabled": "0",
+  "faqEnabled": "1",
+  "policy_url": "https://learnandbuild.in/privacy-policy/"
+};
 
 
 
