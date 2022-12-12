06fbdb7470f33d3787485869b70b8af6432f49c1
silicon <santoshraj175@gmail.com>
2022-12-12 17:37:19 +0530

    change menu icon

--- lib/utils/config.dart
+++ lib/utils/config.dart
@@ -53,7 +53,6 @@ const APK_DETAILS = {
   "policy_url": "https://qa.learningoxygen.com/policy"
 };
 
-
 // const APK_DETAILS = {
 //   "app_name": "learn_build",
 //   "package_name": "com.learn_build",
