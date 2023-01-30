936dede793d0b73032faa9b4f78e3fac8791070d
silicon <santoshraj175@gmail.com>
2023-01-30 18:40:53 +0530

    change menu icon

--- lib/pages/ghome/home_page.dart
+++ lib/pages/ghome/home_page.dart
@@ -16,6 +16,7 @@ import 'package:masterg/pages/gcarvaan/post/gcarvaan_post_page.dart';
 import 'package:masterg/pages/ghome/g_school.dart';
 import 'package:masterg/pages/ghome/ghome.dart';
 import 'package:masterg/pages/reels/reels_dashboard_page.dart';
+import 'package:masterg/pages/singularis/community/commiunity_dashboard.dart';
 import 'package:masterg/pages/singularis/competition.dart';
 import 'package:masterg/pages/singularis/dashboard.dart';
 import 'package:masterg/pages/singularis/dashboard_temp.dart';
