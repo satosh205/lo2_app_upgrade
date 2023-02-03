58d3845ae67ee1ae5cf6e013cc145f81276041dd
Sanjay Maddheshiya <coder.sanjay19@gmail.com>
2023-02-03 12:00:19 +0530

    formated code

--- lib/pages/user_profile_page/portfolio_create_form/extra_activities_list.dart
+++ lib/pages/user_profile_page/portfolio_create_form/extra_activities_list.dart
@@ -16,7 +16,8 @@ import 'package:masterg/utils/resource/colors.dart';
 
 class ExtraActivitiesList extends StatefulWidget {
   final List<CommonProfession> activities;
-  const ExtraActivitiesList({Key? key, required this.activities}) : super(key: key);
+  const ExtraActivitiesList({Key? key, required this.activities})
+      : super(key: key);
 
   @override
   State<ExtraActivitiesList> createState() => _ExtraActivitiesListState();
@@ -30,11 +31,13 @@ class _ExtraActivitiesListState extends State<ExtraActivitiesList> {
         initState: (value) {},
         child: BlocListener<HomeBloc, HomeState>(
             listener: (context, state) async {
-              if (state is SingularisDeletePortfolioState) handleSingularisDeletePortfolioState(state);
+              if (state is SingularisDeletePortfolioState)
+                handleSingularisDeletePortfolioState(state);
             },
-   child: Scaffold(
-      appBar: AppBar(
-          title: Text("Extra currricular Activities",style: Styles.bold()),
+            child: Scaffold(
+                appBar: AppBar(
+                  title: Text("Extra currricular Activities",
+                      style: Styles.bold()),
                   elevation: 0,
                   backgroundColor: ColorConstants.WHITE,
                   leading: IconButton(
@@ -43,8 +46,8 @@ class _ExtraActivitiesListState extends State<ExtraActivitiesList> {
                         Icons.arrow_back_ios,
                         color: ColorConstants.BLACK,
                       )),
-                      actions: [
-                         IconButton(
+                  actions: [
+                    IconButton(
                         onPressed: () async {
                           await showModalBottomSheet(
                               shape: RoundedRectangleBorder(
@@ -68,119 +71,136 @@ class _ExtraActivitiesListState extends State<ExtraActivitiesList> {
                           color: ColorConstants.BLACK,
                         )),
                   ],
-                      
-    ),
-    body: ScreenWithLoader(
-      isLoading:isActivitieLoading,
-      body: Padding(
-        padding: const EdgeInsets.symmetric(vertical:8.0),
-        child: Container(
-          height: height(context)*0.9,
-          width: width(context),
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: ListView.builder(
-              itemCount: 1,
-              itemBuilder: (BuildContext context, int index) { 
-               return Container(
-                        margin: EdgeInsets.only(right: 10),
-                        child: SingleChildScrollView(
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.start,
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Row(
-                                mainAxisAlignment: MainAxisAlignment.start,
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  SizedBox(
-                                    width: width(context) * 0.2,
-                                    height: width(context) * 0.2,
-                                    // child: CachedNetworkImage(
-                                    //   imageUrl:
-                                    //       "${portfolioResponse?.data.baseFileUrl}${extraActivities?[index].imageName}",
-                                    //   progressIndicatorBuilder:
-                                    //       (context, url, downloadProgress) =>
-                                    //           CircularProgressIndicator(
-                                    //               value: downloadProgress.progress),
-                                    //   errorWidget: (context, url, error) => Container(
-                                    //       width: width(context) * 0.2,
-                                    //       height: width(context) * 0.2,
-                                    //       padding: EdgeInsets.all(8),
-                                    //       decoration: BoxDecoration(
-                                    //           color: ColorConstants.DIVIDER,
-                                    //           borderRadius: BorderRadius.circular(8)),
-                                          child: SvgPicture.asset(
-                                              'assets/images/extra.svg')),
-                                              SizedBox(width: 5,),
-                                    
-                                  
-                                   Column(
-                                      crossAxisAlignment: CrossAxisAlignment.start,
-                                      mainAxisAlignment: MainAxisAlignment.start,
-                                      children: [
-                                        Row(
+                ),
+                body: ScreenWithLoader(
+                  isLoading: isActivitieLoading,
+                  body: Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 8.0),
+                    child: Container(
+                        height: height(context) * 0.9,
+                        width: width(context),
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: ListView.builder(
+                              itemCount: 1,
+                              itemBuilder: (BuildContext context, int index) {
+                                return Container(
+                                    margin: EdgeInsets.only(right: 10),
+                                    child: SingleChildScrollView(
+                                      child: Column(
+                                          mainAxisAlignment:
+                                              MainAxisAlignment.start,
+                                          crossAxisAlignment:
+                                              CrossAxisAlignment.start,
                                           children: [
-                                            SizedBox(
-                                              width: width(context)*0.5,
-                                              child: Text(
-                                                maxLines:2,
-                                                
-                                                // '${extraActivities?[index].title}',
-                                               'Man of Match in Interstate Cricket  Tournament', style: Styles.bold(size: 14),
-                                              ),
-                                            ),
-                                            SizedBox(width: 30,),
-     SvgPicture.asset('assets/images/edit_portfolio.svg'),
-                         SizedBox(width: 10,),
-                          SvgPicture.asset('assets/images/delete.svg'),
-                                          ],
-                                        ),
-                                        SizedBox(
-                                          height: 4,
-                                        ),
-                                        Text(
-                                         
-                                         'unocorn pvt',
-                                          style: Styles.regular(size: 14),
-                                        ),
-                                        SizedBox(
-                                          height: 4,
-                                        ),
-                                        // Text('${extraActivities?[index].title}'),
-                                        Text("man of the match in cricket turnament")
-                                      ],
-                                    
-                                  
-                                
-                              
-                            //   SizedBox(
-                            //     height: 4,
-                            //   ),
-                            // //   ReadMoreText(
-                            //     viewMore: 'View more',
-                            //     // text: '${extraActivities?[index].description}',
-                            //     color: Color(0xff929BA3),
-                            //   ),
-                            //   // if (index != extraActivities?.length) Divider()
-                            // ],
-                                   )])]),
-                        )
-        );}),
-          )),
-      ),
-    )
-        ))
-        );
+                                            Row(
+                                                mainAxisAlignment:
+                                                    MainAxisAlignment.start,
+                                                crossAxisAlignment:
+                                                    CrossAxisAlignment.start,
+                                                children: [
+                                                  SizedBox(
+                                                      width:
+                                                          width(context) * 0.2,
+                                                      height:
+                                                          width(context) * 0.2,
+                                                      // child: CachedNetworkImage(
+                                                      //   imageUrl:
+                                                      //       "${portfolioResponse?.data.baseFileUrl}${extraActivities?[index].imageName}",
+                                                      //   progressIndicatorBuilder:
+                                                      //       (context, url, downloadProgress) =>
+                                                      //           CircularProgressIndicator(
+                                                      //               value: downloadProgress.progress),
+                                                      //   errorWidget: (context, url, error) => Container(
+                                                      //       width: width(context) * 0.2,
+                                                      //       height: width(context) * 0.2,
+                                                      //       padding: EdgeInsets.all(8),
+                                                      //       decoration: BoxDecoration(
+                                                      //           color: ColorConstants.DIVIDER,
+                                                      //           borderRadius: BorderRadius.circular(8)),
+                                                      child: SvgPicture.asset(
+                                                          'assets/images/extra.svg')),
+                                                  SizedBox(
+                                                    width: 5,
+                                                  ),
+                                                  Column(
+                                                    crossAxisAlignment:
+                                                        CrossAxisAlignment
+                                                            .start,
+                                                    mainAxisAlignment:
+                                                        MainAxisAlignment.start,
+                                                    children: [
+                                                      Row(
+                                                        children: [
+                                                          SizedBox(
+                                                            width:
+                                                                width(context) *
+                                                                    0.5,
+                                                            child: Text(
+                                                              maxLines: 2,
 
-                
-              }
-              void deletePortfolio(int id) {
-    BlocProvider.of<HomeBloc>(context).add(SingularisDeletePortfolioEvent(portfolioId: id));
-       }
-       void handleSingularisDeletePortfolioState(SingularisDeletePortfolioState state){
+                                                              // '${extraActivities?[index].title}',
+                                                              'Man of Match in Interstate Cricket  Tournament',
+                                                              style:
+                                                                  Styles.bold(
+                                                                      size: 14),
+                                                            ),
+                                                          ),
+                                                          SizedBox(
+                                                            width: 30,
+                                                          ),
+                                                          SvgPicture.asset(
+                                                              'assets/images/edit_portfolio.svg'),
+                                                          SizedBox(
+                                                            width: 10,
+                                                          ),
+                                                          SvgPicture.asset(
+                                                              'assets/images/delete.svg'),
+                                                        ],
+                                                      ),
+                                                      SizedBox(
+                                                        height: 4,
+                                                      ),
+                                                      Text(
+                                                        'unocorn pvt',
+                                                        style: Styles.regular(
+                                                            size: 14),
+                                                      ),
+                                                      SizedBox(
+                                                        height: 4,
+                                                      ),
+                                                      // Text('${extraActivities?[index].title}'),
+                                                      Text(
+                                                          "man of the match in cricket turnament")
+                                                    ],
 
-   
+                                                    //   SizedBox(
+                                                    //     height: 4,
+                                                    //   ),
+                                                    // //   ReadMoreText(
+                                                    //     viewMore: 'View more',
+                                                    //     // text: '${extraActivities?[index].description}',
+                                                    //     color: Color(0xff929BA3),
+                                                    //   ),
+                                                    //   // if (index != extraActivities?.length) Divider()
+                                                    // ],
+                                                  )
+                                                ])
+                                          ]),
+                                    ));
+                              }),
+                        )),
+                  ),
+                ))));
+  }
+
+  void deletePortfolio(int id) {
+    BlocProvider.of<HomeBloc>(context)
+        .add(SingularisDeletePortfolioEvent(portfolioId: id));
+  }
+
+  void handleSingularisDeletePortfolioState(
+      SingularisDeletePortfolioState state) {
     setState(() {
       switch (state.apiState) {
         case ApiStatus.LOADING:
@@ -191,7 +211,7 @@ class _ExtraActivitiesListState extends State<ExtraActivitiesList> {
         case ApiStatus.SUCCESS:
           Log.v("Success Delete  Activities....................");
           isActivitieLoading = false;
- 
+
           Navigator.pop(context);
           break;
         case ApiStatus.ERROR:
@@ -203,7 +223,5 @@ class _ExtraActivitiesListState extends State<ExtraActivitiesList> {
           break;
       }
     });
-
   }
 }
-
