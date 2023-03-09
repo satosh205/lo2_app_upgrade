251b1e6c14bb596d311186bbb316e6c8108153ed
silicon <santoshraj175@gmail.com>
2023-03-09 20:03:18 +0530

    Login password change

--- lib/pages/auth_pages/self_details_page.dart
+++ lib/pages/auth_pages/self_details_page.dart
@@ -500,6 +500,7 @@ class _SelfDetailsPageState extends State<SelfDetailsPage>
               SizedBox(
                 height: 20,
               ),
+
               ///Email Fields
               Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
